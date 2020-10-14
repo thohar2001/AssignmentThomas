ab0cb07b65e9aabec58e0eff4567d39a85fea260
Thomas <thohar2001@skola.goteborg.se>
2020-10-14 12:41:33 +0200

    Adding a main class called GuessingGame.java

---
+++ GuessingGame.java
@@ -0,0 +1,9 @@
+public class GuessingGame {
+    /**
+     * In this class we have a main method where we create an instance of
+     * the Guesser class with two values, 0 and 1000.       
+     **/
+    public static void main(String[] args) {
+        Guesser guess = new Guesser(0, 1000);
+    }
+}
