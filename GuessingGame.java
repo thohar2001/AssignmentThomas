8a0250677a68b7a3f351f8b30587088256dd6450
Thomas <thohar2001@skola.goteborg.se>
2020-10-16 13:30:01 +0200

    Changed GuessingGame.java so that the user has to think of a number between 0 and 10 instead for faster testing

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,10 +1,10 @@
 public class GuessingGame {
     /**
      * In this class we have a main method where we create an instance of
-     * the Guesser class with two values, 0 and 1000.       
+     * the Guesser class with two values, 0 and 10.       
      **/
     public static void main(String[] args) {
-        Guesser guess = new Guesser(0, 1000);
+        Guesser guess = new Guesser(0, 10);
         guess.start();
     }
 }
